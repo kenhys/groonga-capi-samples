f5b38aef6b917adf82318f3d5d96716f9d9f154e
Kentaro Hayashi <hayashi@clear-code.com>
2019-04-18 18:53:46 +0900

    Add query flags and grn_p examples

---
+++ grn_inspect/grn_inspect_query_log_flags.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <groonga.h>
+
+int
+main (int argc, char **argv)
+{
+  grn_ctx context;
+  
+  grn_init();
+  grn_ctx_init(&context, 0);
+
+  grn_obj buffer;
+  GRN_TEXT_INIT(&buffer, 0);
+  int current_flags = grn_query_logger_get_flags(&context);
+  grn_inspect_query_log_flags(&context, &buffer, current_flags);
+  printf("FLAGS: <%.*s>\n", (int)GRN_TEXT_LEN(&buffer), GRN_TEXT_VALUE(&buffer));
+
+  GRN_OBJ_FIN(&context, &buffer);
+  
+  grn_ctx_fin(&context);
+  grn_fin();
+
+  return 0;
+}
