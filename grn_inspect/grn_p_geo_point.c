54ac24383c348185a7fb5185e969e68c42aabf04
Kentaro Hayashi <hayashi@clear-code.com>
2019-04-23 14:11:27 +0900

    Add more grn_p_geo_point example

--- grn_inspect/grn_p_geo_point.c
+++ grn_inspect/grn_p_geo_point.c
@@ -21,6 +21,12 @@ main (int argc, char **argv)
   GRN_GEO_POINT_SET(&context, &point, latitude, longitude);
   grn_p_geo_point(&context, (grn_geo_point*)&point);
 
+  latitude = ((40 * 60 * 60) + (42 * 60) + 46) * 1000;
+  longitude = ((-74 * 60 * 60) + (0 * 60) + 22) * 1000;
+  GRN_WGS84_GEO_POINT_INIT(&point, 0);
+  GRN_GEO_POINT_SET(&context, &point, latitude, longitude);
+  grn_p_geo_point(&context, (grn_geo_point*)&point);
+
   grn_ctx_fin(&context);
   grn_fin();
 
