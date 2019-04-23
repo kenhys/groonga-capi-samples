fc0ce0ef7040e1f393ffea63a9e4ee4a0a55523e
Kentaro Hayashi <hayashi@clear-code.com>
2019-04-23 14:04:05 +0900

    Add geo point comment

--- grn_inspect/grn_p_geo_point.c
+++ grn_inspect/grn_p_geo_point.c
@@ -11,8 +11,10 @@ main (int argc, char **argv)
   grn_init();
   grn_ctx_init(&context, 0);
 
-  int latitude = (45 * 60 * 60 + 0 * 60) * 1000;
-  int longitude = (90 * 60 * 60 + 0 * 60) * 1000;
+  // https://www.clear-code.com/blog/2011/9/13.html
+  // Tokyo station
+  int latitude = ((35 * 60 * 60) + (40 * 60) + 52.975) * 1000;
+  int longitude = ((139 * 60 * 60) + (45 * 60) + 57.902) * 1000;
 
   grn_obj point;
   GRN_WGS84_GEO_POINT_INIT(&point, 0);
