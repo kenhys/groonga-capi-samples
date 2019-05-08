27a7e4c5b4554ade5bd7cb62fcc674f5de2528ed
Kentaro Hayashi <hayashi@clear-code.com>
2019-05-08 13:02:18 +0900

    Add grn_obj_is_column

---
+++ grn_obj/grn_obj_is_column.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <groonga.h>
+
+int
+main(void)
+{
+  grn_ctx context;
+  grn_obj *db;
+  grn_obj *obj;
+  
+  grn_init();
+  grn_ctx_init(&context, 0);
+  db = grn_db_open(&context, "./testdb/db");
+
+  const char *obj_names[] = {
+    "Diaries.title",
+    "Diaries",
+    NULL
+  };
+
+  int i = 0;
+  while (obj_names[i]) {
+    grn_obj buffer;
+    GRN_TEXT_INIT(&buffer, 0);
+    obj = grn_ctx_get(&context, obj_names[i], strlen(obj_names[i]));
+    printf("<%s> column?: <%s>\n",
+           obj_names[i], grn_obj_is_column(&context, obj) ? "true" : "false");
+    GRN_OBJ_FIN(&context, &buffer);
+    i++;
+  }
+  
+  grn_db_unmap(&context, db);
+  grn_ctx_fin(&context);
+  grn_fin();
+
+  return 0;
+}
