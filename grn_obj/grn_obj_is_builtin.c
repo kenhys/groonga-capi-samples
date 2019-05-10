b110d2cf27fcf80211128f33387b53959866366d
Kentaro Hayashi <hayashi@clear-code.com>
2019-05-10 14:54:55 +0900

    Support command line argument

--- grn_obj/grn_obj_is_builtin.c
+++ grn_obj/grn_obj_is_builtin.c
@@ -4,7 +4,7 @@
 #include <groonga.h>
 
 int
-main(void)
+main(int argc, char *argv[])
 {
   grn_ctx context;
   grn_obj *db;
@@ -13,6 +13,15 @@ main(void)
   grn_init();
   grn_ctx_init(&context, 0);
   db = grn_db_open(&context, "./testdb/db");
+
+  if (argc == 2) {
+    obj = grn_ctx_get(&context, argv[1], strlen(argv[1]));
+    if (obj) {
+      printf("<%s> builtin?: <%s>\n",
+             argv[1], grn_obj_is_builtin(&context, obj) ? "true" : "false");
+    }
+    goto exit;
+  }
 
   const char *obj_names[] = {
     "Int8",
@@ -30,7 +39,7 @@ main(void)
     GRN_OBJ_FIN(&context, &buffer);
     i++;
   }
-  
+exit:  
   grn_db_unmap(&context, db);
   grn_ctx_fin(&context);
   grn_fin();
