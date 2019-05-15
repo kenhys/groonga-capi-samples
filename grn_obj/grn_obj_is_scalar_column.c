38cb58c3035df41f5e5e438b48e84187271c3909
Kentaro Hayashi <hayashi@clear-code.com>
2019-05-15 14:57:36 +0900

    Add scalar/vector column examples

---
+++ grn_obj/grn_obj_is_scalar_column.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <groonga.h>
+
+int
+main(void)
+{
+  grn_ctx context, *ctx;
+  grn_obj *db;
+  grn_obj *obj;
+  
+  grn_init();
+  ctx = &context;
+  grn_ctx_init(ctx, 0);
+  db = grn_db_open(ctx, "./testdb/scalar_column");
+
+  const char *obj_names[] = {
+    "Site.user",
+    NULL
+  };
+
+  int i = 0;
+  while (obj_names[i]) {
+    obj = grn_ctx_get(&context, obj_names[i], strlen(obj_names[i]));
+    printf("<%s> data column?: <%s>\n",
+           obj_names[i], grn_obj_is_scalar_column(ctx, obj) ? "true" : "false");
+    i++;
+  }
+  
+  grn_db_unmap(ctx, db);
+  grn_ctx_fin(ctx);
+  grn_fin();
+
+  return 0;
+}
