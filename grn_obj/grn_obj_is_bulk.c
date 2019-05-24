1c7047350f9892f4a6dfd9a2edce46290bf03fc6
Kentaro Hayashi <hayashi@clear-code.com>
2019-05-24 18:42:10 +0900

    Add bulk samples

---
+++ grn_obj/grn_obj_is_bulk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <groonga.h>
+
+int
+main(void)
+{
+  grn_ctx context, *ctx;
+  grn_obj *db;
+  grn_obj obj;
+  
+  grn_init();
+  ctx = &context;
+  grn_ctx_init(ctx, 0);
+  db = grn_db_open(ctx, "./testdb/lexicon");
+
+  GRN_INT32_INIT(&obj, 0);
+  printf("bulk?: <%s>\n", grn_obj_is_bulk(ctx, &obj) ? "true" : "false");
+
+  grn_db_unmap(ctx, db);
+  grn_ctx_fin(ctx);
+  grn_fin();
+
+  return 0;
+}
